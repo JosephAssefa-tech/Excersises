85ee676dfed82e3395b516d9d47a0e1b6aa27622
Joseph <yosephassefa85@gmail.com>
2022-01-26 11:11:55 +0300

    Interface excersie

---
+++ Excersises/InheritanceVsInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersises
+{
+    public interface Car
+    {
+        void Drive();
+       
+        
+    }
+    class Demo : Car
+    {
+        void Car.Drive()
+        {
+            Console.WriteLine("Drive car");
+        }
+
+
+        public static void Main()
+        {
+            Demo demo_object = new Demo();
+            ((Car)demo_object).Drive();
+
+        }
+    }
+}
---
+++ Exe/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exe
+{
+    interface Car
+    {
+        void Drive();
+
+    }
+         class Inherite:Car
+    {
+        void Car.Drive()
+        {
+            Console.WriteLine("Drive a Car "+"\n"+"Excute finish");
+        }
+
+
+        static void Main(string[] args)
+        {
+            //DemoObject.Drive();
+            // Error: Cannot call explicitly implemented interface method
+            // using the class object.
+            // Type cast the demo object to interface type Car
+            Inherite inherit_program = new Inherite();
+            ((Car)inherit_program).Drive();
+            
+        }
+    }
+}

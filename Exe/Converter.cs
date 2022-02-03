4ae551697c9a29f24a49f8bf6be162ecd6365e6d
Joseph <yosephassefa85@gmail.com>
2022-02-03 16:22:34 +0300

    static class example for conversion

---
+++ Exe/Converter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe
+{
+   public static class Converter
+    {
+        public static double toFaranite(double celcious)
+        {
+            return (celcious * 9 / 5) + 32;
+        }
+        public static double toCelcious(double farhanite)
+        {
+            return (farhanite - 32) * 5 / 9;
+        }
+    }
+}
--- Exe/Program.cs
+++ Exe/Program.cs
@@ -2,28 +2,24 @@
 
 namespace Exe
 {
-    interface Car
-    {
-        void Drive();
 
-    }
-         class Inherite:Car
+         class Calculation
     {
-        void Car.Drive()
-        {
-            Console.WriteLine("Drive a Car "+"\n"+"Excute finish");
-        }
+      
 
 
         static void Main(string[] args)
         {
-            //DemoObject.Drive();
-            // Error: Cannot call explicitly implemented interface method
-            // using the class object.
-            // Type cast the demo object to interface type Car
-            Inherite inherit_program = new Inherite();
-            ((Car)inherit_program).Drive();
-            
+            double celcious = 37;
+            double  farhanite = 98.6;
+            Console.WriteLine("Value of Faranite to Celcious is",
+            Converter.toCelcious(farhanite));
+            Console.WriteLine("value for celcious to faranite is ",
+
+            Converter.toFaranite(celcious));
+
+
+
         }
     }
 }

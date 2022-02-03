b0ae0f0df62f3498be6d6519e229b6d5612f0a81
Joseph <yosephassefa85@gmail.com>
2022-02-03 16:27:59 +0300

    static class excersise

--- Exe/Program.cs
+++ Exe/Program.cs
@@ -13,9 +13,9 @@ namespace Exe
             double celcious = 37;
             double  farhanite = 98.6;
             Console.WriteLine("Value of Faranite to Celcious is",
+            //the below Converter word is the static class name and we will acess the method using the class name.
             Converter.toCelcious(farhanite));
             Console.WriteLine("value for celcious to faranite is ",
-
             Converter.toFaranite(celcious));
 
 
